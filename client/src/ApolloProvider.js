4cca05e4b4373f49f075bc2ee57504a53c1cedce
DrummD <drummd@rocketmail.com>
2022-12-28 12:25:04 -0800

    frontend/1.2

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import App from './App';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { setContext } from 'apollo-link-context';
+
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:5000'
+});
+
+
+//auth link is having trouble with the set context 
+//could be the reason for the white screen ?
+//not the reason 
+
+const authLink = setContext(() => {
+  const token = localStorage.getItem('jwtToken');
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
